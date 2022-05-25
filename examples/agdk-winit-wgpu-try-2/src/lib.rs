61536b5f677d74d6c936a7fa936285069a98d3d8
tshrpl <tshrpl@protonmail.com>
2022-05-25 20:25:28 +0530

    update example: `try-2`

--- examples/agdk-winit-wgpu-try-2/src/lib.rs
+++ examples/agdk-winit-wgpu-try-2/src/lib.rs
@@ -1,6 +1,7 @@
 
 
 
+use cgmath;
 use pollster;
 use winit::{
 	event::*,
@@ -13,11 +14,60 @@ use wgpu::util::DeviceExt;
 use std::{
 	ops::Deref,
 	sync::{ Arc, RwLock },
-	borrow::Cow
+	borrow::Cow,
+	time::{ Duration, Instant }
 };
 
 #[macro_use]
 extern crate log;
+
+// mod texture;
+mod camera;
+
+
+
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct CameraUniform {
+	view_proj: [[f32; 4]; 4],
+	eye: [f32; 3],
+	_padding: f32
+}
+
+impl CameraUniform {
+	fn new() -> Self {
+		use cgmath::SquareMatrix;
+		Self {
+			view_proj: cgmath::Matrix4::identity().into(),
+			eye: [0.0; 3],
+			_padding: 0.0
+		}
+	}
+
+	fn update(&mut self, camera: &camera::Camera, projection: &camera::Projection) {
+		self.eye = camera.position.into();
+		self.view_proj = (projection.calc_matrix() * camera.calc_matrix()).into();
+	}
+}
+
+
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct Uniforms {
+	time: f32
+}
+
+impl Uniforms {
+	fn new() -> Self {
+		Self {
+			time: 0.0
+		}
+	}
+
+	fn update(&mut self, time: &Duration) {
+		self.time = time.as_secs_f32();
+	}
+}
 
 
 
@@ -95,7 +145,15 @@ struct RenderState {
 	index_buffer: wgpu::Buffer,
 	num_indices: u32,
 
-	render_pipeline: wgpu::RenderPipeline
+	render_pipeline: wgpu::RenderPipeline,
+
+	camera_uniform: CameraUniform,
+	camera_buffer: wgpu::Buffer,
+	camera_bind_group: wgpu::BindGroup,
+
+	uniforms: Uniforms,
+	uniform_buffer: wgpu::Buffer,
+	uniform_bind_group: wgpu::BindGroup
 }
 
 impl RenderState {
@@ -139,10 +197,90 @@ impl RenderState {
 		);
 		let num_indices = INDICES.len() as u32;
 
+
+		let camera_uniform = CameraUniform::new();
+
+		let camera_buffer = device.create_buffer_init(
+			&wgpu::util::BufferInitDescriptor{
+				label: Some("Camera Buffer"),
+				contents: bytemuck::cast_slice(&[camera_uniform]),
+				usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST
+			}
+		);
+
+		let camera_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+			entries: &[
+				wgpu::BindGroupLayoutEntry {
+					binding: 0,
+					visibility: wgpu::ShaderStages::VERTEX | wgpu::ShaderStages::FRAGMENT,
+					ty: wgpu::BindingType::Buffer {
+						ty: wgpu::BufferBindingType::Uniform,
+						has_dynamic_offset: false,
+						min_binding_size: None
+					},
+					count: None
+				}
+			],
+			label: Some("camera_bind_group_layout")
+		});
+
+		let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+			layout: &camera_bind_group_layout,
+			entries: &[
+				wgpu::BindGroupEntry {
+					binding: 0,
+					resource: camera_buffer.as_entire_binding()
+				}
+			],
+			label: Some("camera_bind_group")
+		});
+
+
+		let mut uniforms = Uniforms::new();
+
+		let uniform_buffer = device.create_buffer_init(
+			&wgpu::util::BufferInitDescriptor{
+				label: Some("Uniform Buffer"),
+				contents: bytemuck::cast_slice(&[uniforms]),
+				usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST
+			}	
+		);
+
+		let uniform_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+			entries: &[
+				wgpu::BindGroupLayoutEntry {
+					binding: 0,
+					visibility: wgpu::ShaderStages::VERTEX | wgpu::ShaderStages::FRAGMENT,
+					ty: wgpu::BindingType::Buffer {
+						ty: wgpu::BufferBindingType::Uniform,
+						has_dynamic_offset: false,
+						min_binding_size: None
+					},
+					count: None
+				}
+			],
+			label: Some("uniform_bind_group_layout")
+		});
+
+		let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+			layout: &uniform_bind_group_layout,
+			entries: &[
+				wgpu::BindGroupEntry {
+					binding: 0,
+					resource: uniform_buffer.as_entire_binding()
+				}
+			],
+			label: Some("uniform_bind_group")
+		});
+
+
 		trace!("WGPU: creating pipeline layout");
 		let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
 			label: None,
-			bind_group_layouts: &[],
+			bind_group_layouts: &[
+				&camera_bind_group_layout,
+				&uniform_bind_group_layout
+			],
 			push_constant_ranges: &[],
 		});
 
@@ -172,8 +310,18 @@ impl RenderState {
 		Self {
 			device, queue, target_format,
 			num_vertices, vertex_buffer, index_buffer, num_indices,
-			render_pipeline
+			render_pipeline,
+			camera_uniform, camera_buffer, camera_bind_group,
+			uniforms, uniform_buffer, uniform_bind_group
 		}
+	}
+
+
+	fn update(&mut self, app: &App) {
+		self.camera_uniform.update(&app.camera.as_ref().unwrap(), &app.projection.as_ref().unwrap());
+		self.queue.write_buffer(&self.camera_buffer, 0, bytemuck::cast_slice(&[self.camera_uniform]));
+		self.uniforms.update(&app.time.as_ref().unwrap());
+		self.queue.write_buffer(&self.uniform_buffer, 0, bytemuck::cast_slice(&[self.uniforms]));
 	}
 
 
@@ -182,6 +330,8 @@ impl RenderState {
 		surface_state: &SurfaceState,
 		app: &App
 	) -> Result<(), wgpu::SurfaceError> {
+
+		self.update(app);
 
 		let output = surface_state.surface.get_current_texture()?;
 		let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
@@ -206,6 +356,8 @@ impl RenderState {
 			});
 
 			render_pass.set_pipeline(&self.render_pipeline);
+			render_pass.set_bind_group(0, &self.camera_bind_group, &[]);
+			render_pass.set_bind_group(1, &self.uniform_bind_group, &[]);
 			render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
 			render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
 			render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
@@ -230,7 +382,11 @@ struct AppInner {
 struct App {
 	inner: Arc<RwLock<AppInner>>,
 
-	clear_color: wgpu::Color
+	clear_color: wgpu::Color,
+	camera: Option<camera::Camera>,
+	projection: Option<camera::Projection>,
+	camera_controller: Option<camera::CameraController>,
+	time: Option<Duration>
 }
 
 impl App {
@@ -238,6 +394,12 @@ impl App {
 		instance: wgpu::Instance,
 		clear_color: wgpu::Color
 	) -> Self {
+
+		let camera = Some(camera::Camera::new((3.0, 3.0, 3.0), cgmath::Deg(-90.0), cgmath::Deg(-20.0)));
+		let projection = None;
+		let camera_controller = Some(camera::CameraController::new(20.0, 1.0));
+		let time = Some(Duration::from_secs(1));
+
 		Self {
 			inner: Arc::new(RwLock::new(AppInner {
 				instance,
@@ -245,7 +407,9 @@ impl App {
 				surface_state: None,
 				render_state: None
 			})),
-			clear_color
+			clear_color,
+			camera, projection, camera_controller,
+			time
 		}
 	}
 }
@@ -279,7 +443,10 @@ async fn ensure_render_state_for_surface(
 	if app_guard.render_state.is_none() {
 		let swapchain_format = new_surface_state.surface.get_preferred_format(&adapter).expect("Couldn't get preferred swapchain format");
 
-		let render_state = RenderState::new(adapter, swapchain_format).await;
+		let render_state = RenderState::new(
+			adapter,
+			swapchain_format
+		).await;
 		app_guard.render_state = Some(render_state);
 	}
 }
@@ -287,7 +454,7 @@ async fn ensure_render_state_for_surface(
 
 
 async fn resume<T>(
-	app: &App,
+	app: &mut App,
 	event_loop: &EventLoopWindowTarget<T>,
 ) {
 	let new_surface_state = SurfaceState::new(&event_loop, &app.read().unwrap().instance);
@@ -295,14 +462,14 @@ async fn resume<T>(
 	pollster::block_on(ensure_render_state_for_surface(app, &new_surface_state));
 	app.write().unwrap().surface_state = Some(new_surface_state);
 
-	let guard = app.read().unwrap();
+	let guard = app.write().unwrap();
 	let render_state = guard.render_state.as_ref().unwrap();
 	let surface_state = guard.surface_state.as_ref().unwrap();
 	let swapchain_format = render_state.target_format;
 	let window_size = surface_state.window.inner_size();
 	let adapter = guard.adapter.as_ref().unwrap();
 
-	let mut config = wgpu::SurfaceConfiguration {
+	let config = wgpu::SurfaceConfiguration {
 		usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
 		format: surface_state.surface.get_preferred_format(&adapter).unwrap(),
 		width: window_size.width,
@@ -310,34 +477,43 @@ async fn resume<T>(
 		present_mode: wgpu::PresentMode::Fifo
 	};
 	surface_state.surface.configure(&render_state.device, &config);
-
 	surface_state.window.request_redraw();
+	drop(guard);
+	app.projection = Some(camera::Projection::new(config.width, config.height, cgmath::Deg(45.0), 0.1, 100.0));
 }
 
 
 
 async fn run(
-	app: App,
+	mut app: App,
 	event_loop: EventLoop<()>
 ) {
+	let mut last_time_instant = Instant::now();
+
 	event_loop.run(move |event, event_loop, control_flow| {
 
 		*control_flow = ControlFlow::Wait;
 		match event {
 			Event::NewEvents(winit::event::StartCause::Init) => {
 				#[cfg(not(target_os="android"))]
-				pollster::block_on(resume(&app, event_loop))
-			}
+				pollster::block_on(resume(&mut app, event_loop))
+			},
 			Event::Resumed => {
-				pollster::block_on(resume(&app, event_loop))
+				pollster::block_on(resume(&mut app, event_loop))
 			},
 			Event::Suspended => {
 				let mut guard = app.write().unwrap();
 				guard.render_state = None;
 			},
 			Event::RedrawRequested(_) => {
+				let mut time = app.time.unwrap();
+				time += last_time_instant.elapsed();
+				last_time_instant = Instant::now();
+				println!("{:?}", time);                 // theres a print here, should print for every frame
+
 				let mut guard = app.write().unwrap();
 				let guard = &mut *guard;
+
 				if let Some(surface_state) = &guard.surface_state {
 					if let Some(render_state) = &mut guard.render_state {
 						match render_state.render(&surface_state, &app) {
@@ -349,10 +525,10 @@ async fn run(
 					}
 				}
 			},
-			Event::RedrawEventsCleared => {
-				let mut guard = app.read().unwrap();
-				guard.surface_state.as_ref().unwrap().window.request_redraw();
-			},
+			// Event::RedrawEventsCleared => {
+			// 	let mut guard = app.read().unwrap();
+			// 	guard.surface_state.as_ref().unwrap().window.request_redraw();
+			// },
 			Event::WindowEvent {
 				event: WindowEvent::CloseRequested,
 				..
@@ -370,7 +546,7 @@ fn _main() {
 	let event_loop = EventLoop::new();
 
 	let instance = wgpu::Instance::new(wgpu::Backends::all());
-	let app = App::new(instance, wgpu::Color::BLUE);
+	let mut app = App::new(instance, wgpu::Color::BLUE);
 
 	pollster::block_on(run(app, event_loop));
 
@@ -393,7 +569,7 @@ fn main() {}
 
 #[cfg(not(target_os="android"))]
 fn main() {
-	simple_logger::SimpleLogger::new().init().unwrap();
+	simple_logger::SimpleLogger::new().with_level(log::LevelFilter::Error).init().unwrap();
 
 	_main();
 }
--- examples/agdk-winit-wgpu-try-2/src/shader.wgsl
+++ examples/agdk-winit-wgpu-try-2/src/shader.wgsl
@@ -1,27 +1,45 @@
 struct VertexInput {
-    [[location(0)]] position: vec2<f32>;
+	[[location(0)]] position: vec2<f32>;
 };
 
 struct VertexOutput {
-    [[builtin(position)]] clip_position: vec4<f32>;
-    [[location(1)]] uv: vec2<f32>;
+	[[builtin(position)]] clip_position: vec4<f32>;
+	[[location(1)]] uv: vec2<f32>;
 };
+
+struct CameraUniform {
+	view_proj: mat4x4<f32>;
+	eye: vec3<f32>;
+};
+
+[[group(0), binding(0)]]
+var<uniform> camera: CameraUniform;
+
+struct Uniforms {
+	time: f32;
+};
+
+[[group(1), binding(0)]]
+var<uniform> uniforms: Uniforms;
 
 
 
 [[stage(vertex)]]
 fn vs_main(model: VertexInput) -> VertexOutput {
-    var out: VertexOutput;
+	var out: VertexOutput;
 
-    out.clip_position = vec4<f32>(model.position.x, model.position.y, 0.0, 1.0);
-    out.uv = model.position;
+	out.clip_position = vec4<f32>(model.position.x, model.position.y, 0.0, 1.0);
+	out.uv = model.position;
 
-    return out;
+	return out;
 }
 
 
 
 [[stage(fragment)]]
 fn fs_main(in: VertexOutput) -> [[location(0)]] vec4<f32> {
-    return vec4<f32>(in.uv.x, in.uv.y, 0.0, 1.0);
+	// return vec4<f32>(in.uv.x, in.uv.y, 0.0, 1.0);
+
+	var u_time: f32 = uniforms.time;
+	return vec4<f32>(sin(u_time), 0.0, 0.0, 1.0);
 }
